db33b5d7f6d71aaf7fc1ea3bd2712cada1b821f5
Shushu9 <pokotilo.gleb1@gmail.com>
2020-10-03 14:08:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  console.log(array);
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      return Math.min.apply(null, array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  console.log(array);
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      return Math.max.apply(null, array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  console.log(array);
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      return array.reduce((current, next) => current + next, 0) / array.length;
+  }
 }
